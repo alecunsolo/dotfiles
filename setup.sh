684a41206baf2bd286b5435b94ee187e5f7f68f7
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2021-04-02 12:29:36 +0200

    Added custom plugin/themes

--- setup.sh
+++ setup.sh
@@ -11,4 +11,8 @@ else
 fi
 
 # powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# completion
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-completions
+# syntax
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
