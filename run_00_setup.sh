45212a91556a8bc51e291fd57fa341f0cb6e24d1
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2021-04-02 14:55:30 +0200

    Initial setup script

---
+++ run_00_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# oh-my-zsh
+ZSH=${ZSH:-~/.oh-my-zsh}
+if [ -d "$ZSH" ]; then
+    echo "oh-my-zsh already installed in $ZSH. Nothing to do"
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
