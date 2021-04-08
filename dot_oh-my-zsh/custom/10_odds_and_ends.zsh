84b6a82126ce00bcf032983554acb8bfaf5ffad9
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2021-04-08 10:14:42 +0200

    Added odds and ends stuff for zsh

---
+++ dot_oh-my-zsh/custom/10_odds_and_ends.zsh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# Start or attach to a tmux session with the given name ('main' as default)
+function txa() {
+    tmux new-session -A -s ${1:-main}
+}
+
+alias tmuxp='PYENV_VERSION=tmuxp tmuxp'
+
+# Create a temporary directory and go there
+function mkdt() {
+    local dir
+    dir=$(mktemp -d)
+    cd $dir
+}
+
+# External ip
+function ext-ip() {
+    if (( ${+commands[dig]} )); then
+        dig +short myip.opendns.com @resolver1.opendns.com
+        return $?
+    else
+        print "dig not installed" >&2
+        return 1
+    fi
+}
