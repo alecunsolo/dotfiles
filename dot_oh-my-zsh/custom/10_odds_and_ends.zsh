1fbe40266b8440c0da6335028af9dbf1d9fb6623
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2021-07-09 12:35:07 +0200

    Added ssh-reagent function

--- dot_oh-my-zsh/custom/10_odds_and_ends.zsh
+++ dot_oh-my-zsh/custom/10_odds_and_ends.zsh
@@ -32,3 +32,17 @@ alias -s {yaml,json}=$EDITOR
 if (( ${+commands[bat]} )); then
     alias -g B=' | bat'
 fi
+
+# Reconnect to ssh-agent. Use with caution
+# See: https://tychoish.com/post/9-awesome-ssh-tricks/
+function ssh-reagent () {
+  for agent in /tmp/ssh-*/agent.*; do
+      export SSH_AUTH_SOCK=$agent
+      if ssh-add -l 2>&1 > /dev/null; then
+         echo "Found working SSH Agent:"
+         ssh-add -l
+         return
+      fi
+  done
+  echo "Cannot find ssh agent - maybe you should reconnect and forward it?"
+}
