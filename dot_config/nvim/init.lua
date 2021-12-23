9625c78a0b3a7297cd33ff6b57e2073473c7b0ef
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2021-12-23 19:06:09 +0100

    Simplified plugin management

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,23 @@
 require('options')
 require('mappings')
 require('plugins')
+
+-- This may fail on bootstrap
+local configurations = {
+  'completion',
+  'gitsigns',
+  'lsp',
+  'lualine',
+  'nightfox',
+  'telescope',
+  'tmux',
+  'treesitter',
+}
+
+for _, plugin in ipairs(configurations) do
+  local status_ok, _ = pcall(require, 'config.'..plugin)
+  if not status_ok then
+    vim.notify('Failed to configure plugin '..plugin)
+  end
+end
+
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -16,64 +16,51 @@ end
 
 return packer.startup(function(use)
   -- Self manage
-  use({'https://github.com/wbthomason/packer.nvim'})
+  use 'https://github.com/wbthomason/packer.nvim'
   -- Color scheme
-  use({'https://github.com/EdenEast/nightfox.nvim', config = [[require('config.nightfox')]]})
+  use 'https://github.com/EdenEast/nightfox.nvim'
   -- Lualine
-  use({'https://github.com/nvim-lualine/lualine.nvim',
-      requires = {'https://github.com/kyazdani42/nvim-web-devicons'},
-      config = [[require('config.lualine')]]
-  })
+  use 'https://github.com/nvim-lualine/lualine.nvim'
+  use 'https://github.com/kyazdani42/nvim-web-devicons'
 
   -- Git
-  use({'https://github.com/tpope/vim-fugitive'})
-  use({'https://github.com/lewis6991/gitsigns.nvim',
-    requires = {'https://github.com/nvim-lua/plenary.nvim'},
-    config = [[require('config.gitsigns')]]
-  })
+  use 'https://github.com/tpope/vim-fugitive'
+  use 'https://github.com/lewis6991/gitsigns.nvim'
+  use 'https://github.com/nvim-lua/plenary.nvim'
 
   -- Tmux integration
-  use({'https://github.com/alexghergh/nvim-tmux-navigation', config = [[require('config.tmux')]]})
+  use 'https://github.com/alexghergh/nvim-tmux-navigation'
 
   -- LSP
-  use({'https://github.com/neovim/nvim-lspconfig',
-    requires = {'williamboman/nvim-lsp-installer'},
-    config = [[require('config.lsp')]]
-  })
+  use 'https://github.com/neovim/nvim-lspconfig'
+  use 'https://github.com/williamboman/nvim-lsp-installer'
 
   -- Telescope
-  use({ 'https://github.com/nvim-telescope/telescope.nvim',
-    requires = {'https://github.com/nvim-lua/plenary.nvim'},
-    config = [[require('config.telescope')]]
-  })
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use 'https://github.com/nvim-telescope/telescope.nvim'
+  use 'https://github.com/nvim-lua/plenary.nvim'
+  use {'https://github.com/nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   -- Tree sitter
-  use({'https://github.com/nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    config = [[require('config.treesitter')]]
-  })
+  use {'https://github.com/nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
   -- Autocompletion
-  use({'hrsh7th/nvim-cmp',
-    requires = { 'https://github.com/neovim/nvim-lspconfig',
-      'https://github.com/hrsh7th/cmp-nvim-lsp',
-      'https://github.com/hrsh7th/cmp-nvim-lua',
-      'https://github.com/hrsh7th/cmp-buffer',
-      'https://github.com/hrsh7th/cmp-path',
-      'https://github.com/hrsh7th/cmp-cmdline',
-      -- Snippets
-      'https://github.com/L3MON4D3/LuaSnip',
-      'https://github.com/rafamadriz/friendly-snippets',
-      'https://github.com/saadparwaiz1/cmp_luasnip'
-    },
-    config = [[require('config.completion')]]
-  })
+  use 'https://github.com/hrsh7th/nvim-cmp'
+  use 'https://github.com/neovim/nvim-lspconfig'
+  use 'https://github.com/hrsh7th/cmp-nvim-lsp'
+  use 'https://github.com/hrsh7th/cmp-nvim-lua'
+  use 'https://github.com/hrsh7th/cmp-buffer'
+  use 'https://github.com/hrsh7th/cmp-path'
+  use 'https://github.com/hrsh7th/cmp-cmdline'
+  -- Snippets
+  use 'https://github.com/L3MON4D3/LuaSnip'
+  use 'https://github.com/rafamadriz/friendly-snippets'
+  use 'https://github.com/saadparwaiz1/cmp_luasnip'
 
   -- Editor config
-  use({'https://github.com/editorconfig/editorconfig-vim'})
+  use 'https://github.com/editorconfig/editorconfig-vim'
 
   if packer_bootstrap then
     require('packer').sync()
   end
 end)
+
