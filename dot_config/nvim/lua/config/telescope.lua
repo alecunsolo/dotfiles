9854021fee93ffb1736bc51a67329e41440761ff
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2021-12-23 11:53:18 +0100

    Add telescope hidden files search

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -1,5 +1,28 @@
 require('config.map')
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    find_files = {
+      find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+    },
+  },
+}
+
+local M = {}
+M.hidden_files = function()
+  local opts = { find_command = { "fd", "--type", "f", "--hidden", "--exclude", ".git", "--strip-cwd-prefix" } }
+  require"telescope.builtin".find_files(opts)
+end
 
 map('n', '<leader>ff', ':Telescope find_files<CR>', opts)
 map('n', '<leader>fg', ':Telescope git_files<CR>', opts)
 map('n', '<leader>fb', ':Telescope buffers<CR>', opts)
+map('n', '<leader>fh', ':lua require("config.telescope").hidden_files()<CR>', opts)
+
+return M
