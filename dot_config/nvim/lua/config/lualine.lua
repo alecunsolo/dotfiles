aad7707c213ec61f9522d9a8a41bd790e337b100
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2021-12-27 15:12:37 +0100

    Full filename in lualine

--- dot_config/nvim/lua/config/lualine.lua
+++ dot_config/nvim/lua/config/lualine.lua
@@ -9,4 +9,17 @@ lualine.setup({
     -- ... your lualine config
     theme = 'nightfox',
   },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+        symbols = {
+          modified = '[+]', -- when the file was modified
+          readonly = '[]', -- if the file is not modifiable or readonly
+          unnamed = '[No Name]', -- default display name for unnamed buffers
+        },
+      },
+    },
+  },
 })
