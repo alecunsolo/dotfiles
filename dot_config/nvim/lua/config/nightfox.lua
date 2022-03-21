a1dd6409fbc282518593db8a2753ea9d4f961252
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2022-03-21 15:03:36 +0100

    Updated nvim plugin config

--- dot_config/nvim/lua/config/nightfox.lua
+++ dot_config/nvim/lua/config/nightfox.lua
@@ -7,12 +7,13 @@ end
 -- This function set the configuration of nightfox. If a value is not passed in the setup function
 -- it will be taken from the default configuration above
 nightfox.setup({
-  fox = 'duskfox',
-  alt_nc = true,
-  styles = {
-    comments = 'italic',
-    keywords = 'bold',
-    functions = 'italic,bold',
-  },
+  options = {
+    dim_inactive = true,
+    styles = {
+      comments = 'italic',
+      keywords = 'bold',
+      functions = 'italic,bold',
+    },
+  }
 })
-nightfox.load()
+vim.cmd("colorscheme duskfox")
--- dot_config/nvim/lua/config/nvim-tree.lua
+++ dot_config/nvim/lua/config/nvim-tree.lua
@@ -1,4 +1,4 @@
-vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
+-- vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
 vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
