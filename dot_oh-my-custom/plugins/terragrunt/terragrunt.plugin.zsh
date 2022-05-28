8b94f400e1014dafafcdf210c6afdc74bae246ef
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2022-05-28 16:59:11 +0200

    Added terragrunt plugin

---
+++ dot_oh-my-custom/plugins/terragrunt/terragrunt.plugin.zsh
@@ -0,0 +1,27 @@
+# If terragrunt is not installed do nothing
+if [[ ! -x "${commands[terragrunt]}" ]]; then
+    return
+fi
+
+alias tg='terragrunt'
+alias tgp='terragrunt plan'
+alias tga='terragrunt apply'
+alias tgi='terragrunt init'
+alias tgf='terragrunt hclfmt'
+
+alias tgpf='terragrunt plan -output terragrunt.plan'
+
+tgaf() {
+    local PLANFILE=terragrunt.plan
+    if ! terragrunt plan -out $PLANFILE ; then
+        echo "Failed to plan"
+        return 1
+    fi
+    if read -t 5 -s -q "RESP?Do you want to apply this? [y/N] " ; then
+        echo "Ok"
+        terragrunt apply $PLANFILE
+    else
+        echo "No harm done"
+        echo "You can manually apply this plan with \"terragrunt apply $PLANFILE\""
+    fi
+}
