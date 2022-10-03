ac4d5c7351ec4b4a2e48326be8dee4b9787e54e8
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2022-10-03 11:16:19 +0200

    Fixed nvim-tree config

--- dot_config/nvim/lua/config/nvim-tree.lua
+++ dot_config/nvim/lua/config/nvim-tree.lua
@@ -1,27 +1,4 @@
 -- vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
-vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
-vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-vim.g.nvim_tree_icons = {
-  default = '',
-  symlink = '',
-  git = {
-    unstaged = '',
-    staged = 'S',
-    unmerged = '',
-    renamed = '➜',
-    deleted = '',
-    untracked = 'U',
-    ignored = '◌',
-  },
-  folder = {
-    default = '',
-    open = '',
-    empty = '',
-    empty_open = '',
-    symlink = '',
-  },
-}
 
 local status_ok, nvim_tree = pcall(require, 'nvim-tree')
 if not status_ok then
@@ -33,4 +10,32 @@ require('config.map')
 
 map('n', '<Leader>e', '<cmd>NvimTreeFindFileToggle<cr>', opts)
 
-nvim_tree.setup()
+nvim_tree.setup({
+  renderer = {
+    highlight_git = true,
+    add_trailing = true,
+    highlight_opened_files = "icon",
+    icons = {
+      glyphs = {
+        default = '',
+        symlink = '',
+        git = {
+          unstaged = '',
+          staged = 'S',
+          unmerged = '',
+          renamed = '➜',
+          deleted = '',
+          untracked = 'U',
+          ignored = '◌',
+        },
+        folder = {
+          default = '',
+          open = '',
+          empty = '',
+          empty_open = '',
+          symlink = '',
+        },
+      }
+    }
+  }
+})
