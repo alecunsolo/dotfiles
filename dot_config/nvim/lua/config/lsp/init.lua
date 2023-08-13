e200c2e974ae3867da08dce74dc8e3e5ded312a9
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2023-08-13 16:51:52 +0200

    Restart nvim from zero

--- dot_config/nvim/lua/config/lsp/init.lua
+++
@@ -1,3 +0,0 @@
--- require('config.lsp.lsp-installer').setup()
--- require('config.lsp.null-ls')
-require('config.lsp.mason')
--- dot_config/nvim/lua/config/map.lua
+++
@@ -1,2 +0,0 @@
-opts = { silent = false, noremap = true }
-map = vim.api.nvim_set_keymap
