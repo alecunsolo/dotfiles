8a5e3c170935faa5684790d14fcaa8b74ab17f95
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2023-08-15 19:15:48 +0200

    ARGH! MY EYES!!!

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,13 +5,6 @@
 -- NB Do this as early as possible
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
--- Set colors for indent-line
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
 -- ###############
 -- # VIM OPTIONS #
@@ -120,18 +113,6 @@ require('lazy').setup({
     opts = {
       show_end_of_line = true,
       show_current_context = true,
-      space_char_blankline = " ",
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-      },
-
-      -- char = '┊',
-      -- show_trailing_blankline_indent = false,
     },
   },
 
