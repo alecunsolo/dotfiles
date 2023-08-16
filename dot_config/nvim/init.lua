09ff4a8f0b497fc6ffa3f11078cf82bacc9da84d
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2023-08-16 15:25:17 +0200

    Added attribution

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,11 @@
+-- A lot of this stuff is copy/pasted from:
+--  - https://github.com/nvim-lua/kickstart.nvim
+--  - https://github.com/ThePrimeagen/init.lua
+
 -- ###################
 -- # GLOBAL SETTINGS #
 -- ###################
--- -- Set <space> as the leader key
+-- Set <space> as the leader key
 -- NB Do this as early as possible
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
