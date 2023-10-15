89689e55fae26602e87d7ab069239d83044d0fd4
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2023-10-15 18:12:53 +0200

    Fixed opacity

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,15 +4,15 @@ local config = {}
 
 -- Fonts
 config.font = wezterm.font_with_fallback({
-  {
-    family = "FiraCode Nerd Font",
-    harfbuzz_features = {
-      "zero",       -- 0
-      "ss05",       -- @
-      "ss03",       -- &
-    }
-  },
-  "JetBrains Mono",
+    {
+        family = "FiraCode Nerd Font",
+        harfbuzz_features = {
+            "zero", -- 0
+            "ss05", -- @
+            "ss03", -- &
+        }
+    },
+    "JetBrains Mono",
 })
 config.font_size = 12
 
@@ -23,5 +23,7 @@ config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = false
 
 config.audible_bell = "Disabled"
+
+config.window_background_opacity = 0.90
 
 return config
