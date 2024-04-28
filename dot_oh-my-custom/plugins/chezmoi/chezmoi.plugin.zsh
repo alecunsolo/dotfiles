fcc16c4d7baacf70f5cff6b32c3e72b8ee7648ce
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2024-04-28 18:09:01 +0200

    Streamlined custom chezmoi plugin

--- dot_oh-my-custom/plugins/chezmoi/chezmoi.plugin.zsh
+++ dot_oh-my-custom/plugins/chezmoi/chezmoi.plugin.zsh
@@ -3,18 +3,6 @@ if [[ ! -x "${commands[chezmoi]}" ]]; then
     return
 fi
 
-# Completion
-__CHEZMOI_COMPLETION_FILE="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/chezmoi_completion"
-mkdir -p ${__CHEZMOI_COMPLETION_FILE:h}
-
-if [[ ! -f $__CHEZMOI_COMPLETION_FILE || ! -s $__CHEZMOI_COMPLETION_FILE ]]; then
-    chezmoi completion zsh >! $__CHEZMOI_COMPLETION_FILE
-fi
-
-[[ -f $__CHEZMOI_COMPLETION_FILE ]] && source $__CHEZMOI_COMPLETION_FILE
-
-unset __CHEZMOI_COMPLETION_FILE
-
 # Aliases
 alias cz='chezmoi'
 
