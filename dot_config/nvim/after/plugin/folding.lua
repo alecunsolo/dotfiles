3af93759603cdd368cdce869055786ec6143ef8f
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2024-05-03 12:21:20 +0200

    Configured code folding in neovim

---
+++ dot_config/nvim/after/plugin/folding.lua
@@ -0,0 +1,6 @@
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
+vim.opt.foldcolumn = "0"
+vim.opt.foldlevel = 99
+vim.keymap.set("n", "+", "za")
