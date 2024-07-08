811d64a8c1ecd35b0167fa747820082c8f942a05
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2024-07-08 20:32:20 +0200

    Added treesitter textobjects and context plugins

--- dot_config/nvim/lua/custom/plugins/treesitter.lua
+++ dot_config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    -- TODO: Look into textobject
-    -- "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-context",
   },
   build = ":TSUpdate",
   config = function()
@@ -24,69 +24,61 @@ return {
         scope_incremental = "grc",
         node_decremental = "grm",
       },
-      -- TODO: Look into textobject
-      -- textobjects = {
-      --   select = {
-      --     enable = true,
-
-      --     -- Automatically jump forward to textobj, similar to targets.vim
-      --     lookahead = true,
-
-      --     keymaps = {
-      --       -- You can use the capture groups defined in textobjects.scm
-      --       ["af"] = "@function.outer",
-      --       ["if"] = "@function.inner",
-      --       ["ac"] = "@class.outer",
-      --       -- You can optionally set descriptions to the mappings (used in the desc parameter of
-      --       -- nvim_buf_set_keymap) which plugins like which-key display
-      --       ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-      --       -- You can also use captures from other query groups like `locals.scm`
-      --       ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-      --     },
-      --     -- You can choose the select mode (default is charwise 'v')
-      --     --
-      --     -- Can also be a function which gets passed a table with the keys
-      --     -- * query_string: eg '@function.inner'
-      --     -- * method: eg 'v' or 'o'
-      --     -- and should return the mode ('v', 'V', or '<c-v>') or a table
-      --     -- mapping query_strings to modes.
-      --     selection_modes = {
-      --       ["@parameter.outer"] = "v", -- charwise
-      --       ["@function.outer"] = "V", -- linewise
-      --       ["@class.outer"] = "<c-v>", -- blockwise
-      --     },
-      --     -- If you set this to `true` (default is `false`) then any textobject is
-      --     -- extended to include preceding or succeeding whitespace. Succeeding
-      --     -- whitespace has priority in order to act similarly to eg the built-in
-      --     -- `ap`.
-      --     --
-      --     -- Can also be a function which gets passed a table with the keys
-      --     -- * query_string: eg '@function.inner'
-      --     -- * selection_mode: eg 'v'
-      --     -- and should return true or false
-      --     include_surrounding_whitespace = true,
-      --   },
-      --   move = {
-      --     enable = true,
-      --     set_jumps = true, -- whether to set jumps in the jumplist
-      --     goto_next_start = {
-      --       ["]f"] = "@function.outer",
-      --       ["]]"] = "@class.outer",
-      --     },
-      --     goto_next_end = {
-      --       ["]F"] = "@function.outer",
-      --       ["]["] = "@class.outer",
-      --     },
-      --     goto_previous_start = {
-      --       ["[f"] = "@function.outer",
-      --       ["[["] = "@class.outer",
-      --     },
-      --     goto_previous_end = {
-      --       ["[F"] = "@function.outer",
-      --       ["[]"] = "@class.outer",
-      --     },
-      --   },
-      -- },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ai"] = "@conditional.outer",
+            ["ii"] = "@conditional.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]x"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]X"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[x"] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[X"] = "@class.outer",
+          },
+          goto_next = {
+            ["]i"] = "@conditional.outer",
+          },
+          goto_previous = {
+            ["[i"] = "@conditional.outer",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+      },
     })
+    -- Context
+    require("treesitter-context").setup({})
   end,
 }
