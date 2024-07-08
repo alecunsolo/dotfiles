c0c4571f16a2ad10baad5a3d59ee192233ff287a
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2024-07-08 20:31:49 +0200

    Added formatter options for terragrunt files

--- dot_config/nvim/lua/custom/plugins/formatter.lua
+++ dot_config/nvim/lua/custom/plugins/formatter.lua
@@ -24,13 +24,10 @@ return { -- Autoformat
         }
       end,
       formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        ["lua"] = { "stylua" },
+        ["terraform-vars"] = { { "terraform_fmt", "tofu_fmt" } },
+        ["terraform"] = { { "terraform_fmt", "tofu_fmt" } },
+        ["hcl"] = { "terragrunt_hclfmt" },
       },
     })
 
