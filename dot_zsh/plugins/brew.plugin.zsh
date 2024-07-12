4b69f4f4771f4ffb58ffa5e2806eabbf9d5799cd
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2024-07-12 19:17:32 +0200

    Fixed brew initialization

--- dot_zsh/plugins/brew.plugin.zsh
+++ dot_zsh/plugins/brew.plugin.zsh
@@ -1,12 +1,37 @@
-# Load standard Homebrew shellenv into the shell session.
-# Load 'HOMEBREW_' prefixed variables only. Avoid loading 'PATH' related
-# variables as they are already handled in standard zsh configuration.
-if (( $+commands[brew] )); then
-    # $(brew shellenv 2> /dev/null) runs the brew shellenv command and gets its output.
-    # (f) splits this output into an array of lines.
-    # (@M) applies pattern matching to the array.
-    # :#export HOMEBREW* filters the array, keeping only those elements that match the pattern export HOMEBREW*.
-    eval "${(@M)${(f)"$(brew shellenv 2> /dev/null)"}:#export HOMEBREW*}"
+if (( ! $+commands[brew] )); then
+  if [[ -n "$BREW_LOCATION" ]]; then
+    if [[ ! -x "$BREW_LOCATION" ]]; then
+      echo "$BREW_LOCATION is not executable"
+      return
+    fi
+  elif [[ -x /opt/homebrew/bin/brew ]]; then
+    BREW_LOCATION="/opt/homebrew/bin/brew"
+  elif [[ -x /usr/local/bin/brew ]]; then
+    BREW_LOCATION="/usr/local/bin/brew"
+  elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+    BREW_LOCATION="/home/linuxbrew/.linuxbrew/bin/brew"
+  elif [[ -x "$HOME/.linuxbrew/bin/brew" ]]; then
+    BREW_LOCATION="$HOME/.linuxbrew/bin/brew"
+  else
+    return
+  fi
+
+  # Only add Homebrew installation to PATH, MANPATH, and INFOPATH if brew is
+  # not already on the path, to prevent duplicate entries. This aligns with
+  # the behavior of the brew installer.sh post-install steps.
+  eval "$("$BREW_LOCATION" shellenv)"
+  unset BREW_LOCATION
+fi
+
+if [[ -z "$HOMEBREW_PREFIX" ]]; then
+  # Maintain compatability with potential custom user profiles, where we had
+  # previously relied on always sourcing shellenv. OMZ plugins should not rely
+  # on this to be defined due to out of order processing.
+  export HOMEBREW_PREFIX="$(brew --prefix)"
+fi
+
+if [[ -d "$HOMEBREW_PREFIX/share/zsh/site-functions" ]]; then
+  (( ${fpath[(Ie)$HOMEBREW_PREFIX/share/zsh/site-functions]} )) || fpath=("$HOMEBREW_PREFIX/share/zsh/site-functions" $fpath)
 fi
 
 # Aliases
