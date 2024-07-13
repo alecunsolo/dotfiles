ad987cd6132b7ace7385683a718fb9e311d1dedb
Alessandro Cunsolo <alessandro.cunsolo@gmail.com>
2024-07-13 16:35:27 +0200

    Reduced transparency in wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 local config = {}
 
@@ -10,13 +10,13 @@ config.font = wezterm.font_with_fallback({
       "zero", -- 0
       "ss05", -- @
       "ss03", -- &
-    }
+    },
   },
   "JetBrains Mono",
 })
 config.font_size = 12
 
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 config.enable_tab_bar = false
 
 config.send_composed_key_when_left_alt_is_pressed = true
@@ -24,6 +24,6 @@ config.send_composed_key_when_right_alt_is_pressed = false
 
 config.audible_bell = "Disabled"
 
-config.window_background_opacity = 0.90
+config.window_background_opacity = 0.95
 
 return config
